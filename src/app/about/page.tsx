fc2d1797883b44d0acb78bb93797f9e820010c23
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-06-04 22:59:46 -0700

    app inital setup compelted

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="mx-w-6xl mx-auto py-12">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+      tempore quia itaque consectetur, sunt minima placeat necessitatibus
+      eligendi, laborum voluptas quam odit tenetur laudantium deleniti sapiente,
+      delectus maxime cupiditate sit.
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        distinctio, deleniti, nam inventore impedit neque commodi nulla natus
+        iste facere quasi repellendus non ullam odio blanditiis et accusantium
+        at atque!
+      </p>
+    </div>
+  );
+};
+
+export default page;
