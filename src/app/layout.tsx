5d4bd413ee93dbec937c860b321e3de389702421
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-06-05 00:23:10 -0700

    next auth basic completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/Header";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,17 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class">
-          <div className="dark:bg-gray-800 dark:text-gray-300 transition-all duration-7000 ease-in-out min-h-screen h-full">
-            <div className="max-w-6xl mx-auto ">
-              <Header />
-              <div className="py-12">{children}</div>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class">
+            <div className="dark:bg-gray-800 dark:text-gray-300 transition-all duration-7000 ease-in-out min-h-screen h-full">
+              <div className="max-w-6xl mx-auto ">
+                <Header />
+                <div className="py-12">{children}</div>
+              </div>
             </div>
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,6 +5,7 @@ import React, { useId, useState } from "react";
 import { Button } from "../ui/button";
 import { Carrot, Moon, SunMoon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { UserButton, useUser } from "@clerk/nextjs";
 const menu = [
   {
     id: 1,
@@ -26,6 +27,7 @@ const menu = [
 const Header = () => {
   const { theme, setTheme } = useTheme();
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const { isLoaded: userloading, user } = useUser();
 
   const handleDropdownToggle = () => {
     setDropdownVisible(!dropdownVisible);
@@ -66,10 +68,22 @@ const Header = () => {
             onClick={changeTheme}
           />
         )}
-
-        <Link href={"/sign-in"}>
-          <Button className="dark:text-white">Get Started</Button>
-        </Link>
+        {!userloading ? (
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        ) : user ? (
+          <UserButton />
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button className="dark:text-white">Get Started</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
